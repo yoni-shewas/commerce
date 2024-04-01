eeb4bee76425ac8e5a2ea3048c072064c89974c3
yoniShewa <yonatanmekuriya@gmail.com>
2024-04-01 18:09:16 +0300

    updated requirment list

--- commerce/settings.py
+++ commerce/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'yoni-shewas.onrender.com'
+]
 
 
 # Application definition
